af453642210dd4e86a1154a710ab0d3eaa4953a9
sanogawa <yu_01_sanogawa@jp.honda>
2025-03-19 20:18:33 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 def add(a, b, c=0):
-   try:
-     return int(a + b) + int(c)
-   except:
-     return print("error")
-   
-add(2, 2) 
+  try:
+    return int(a + b) + int(c)
+  except:
+    return "error"
